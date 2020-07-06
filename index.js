89765af119917b6c456d7f9dca11743dba262ca7
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-06 11:29:04 +0200

    Create debounce function and move into utils.js file

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-async function fetchData () {
+async function fetchData (searchTerm) {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '94dd967b',
-      t: 'chef'
+      s: searchTerm
     }
   });
   console.log(response.data);
 }
 
 
+const input = document.querySelector('input');
+
+
+const onInput = e => {
+  fetchData(e.target.value)
+}
+
+input.addEventListener('input', debounce(onInput, 500))
