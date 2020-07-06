5dfc3ca3421a3c9df97302cc0eaf8021b45a1867
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-06 09:28:38 +0200

    Add function call to fetch API data

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+async function fetchData () {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '94dd967b',
+      t: 'chef'
+    }
+  });
+  console.log(response.data);
+}
+
+
