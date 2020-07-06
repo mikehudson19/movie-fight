25910ab046171f7d0731efc01a99c4e49dc4b130
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-06 16:41:38 +0200

    Add functionality to append results to the DOM.

--- index.js
+++ index.js
@@ -13,21 +13,50 @@ async function fetchData (searchTerm) {
   }
 }
 
+// Creating the structure of the dropdown list
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input"/>
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`
 
+// The UI element variables that we are going to use
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 
-const onInput = async e => {
+
+const onInput = async (e) => {
   const movies = await fetchData(e.target.value);
+
+  // Clear the dropdown each time a search is made
+  resultsWrapper.innerHTML = '';
   
+  // Add the 'is-active' class to display the dropdown menu
+  dropdown.classList.add('is-active');
+
   for (obj of movies) {
-    const div = document.createElement('a');
-    div.className = 'dropdown-item';
-    div.innerHTML = `${obj.Title}`
-    const target = document.querySelector('.dropdown-content');
-    target.appendChild(div);
+    // Create a dropdown item for each element in the 'movies' array
+    const option = document.createElement('a');
+    option.classList.add('dropdown-item');
+    const imgSRC = obj.Poster === 'N/A' ? '' : obj.Poster; 
+    option.innerHTML = `
+      <img src="${imgSRC}"/>
+      ${obj.Title} 
+    `
+    // Append each dropdown item to the UI
+   resultsWrapper.appendChild(option);
   }
+
+  
 
 }
 
-input.addEventListener('input', debounce(onInput, 1500))
+input.addEventListener('input', debounce(onInput, 1000))
+
