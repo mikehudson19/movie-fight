732b94ddbfc1054bc335dc6190017584759a158a
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-07 08:48:13 +0200

    Create function to add movie details to the DOM.

--- index.js
+++ index.js
@@ -89,12 +89,44 @@ const getMoreData = async (movie) => {
       i: movie.imdbID
     }
   })
-  updateUI(response.data);
+  document.querySelector('.movie-info').innerHTML = updateUI(response.data);
 }
 
 function updateUI (movieDetail) {
-  const movieInfo = document.querySelector('.movie-info');
-  movieInfo.innerHTML = `
-
+  return `
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${movieDetail.Poster}"/>
+      </p>
+    </figure>
+  <div class="media-content">
+    <div class="content">
+      <h1>${movieDetail.Title}</h1>
+      <h4>${movieDetail.Genre}</h4>
+      <p>${movieDetail.Plot}</p>
+    </div>
+  </div>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.Awards}</p>
+  <p class="subtitle">Awards</p>
+</article> 
+<article class="notification is-primary">
+  <p class="title">${movieDetail.BoxOffie}</p>
+  <p class="subtitle">Box Office</p>
+</article> 
+<article class="notification is-primary">
+  <p class="title">${movieDetail.Metascore}</p>
+  <p class="subtitle">Metascore</p>
+</article> 
+<article class="notification is-primary">
+  <p class="title">${movieDetail.imdbRating}</p>
+  <p class="subtitle">IMDB Rating</p>
+</article> 
+<article class="notification is-primary">
+  <p class="title">${movieDetail.imdbVotes}</p>
+  <p class="subtitle">IMDB Votes</p>
+</article> 
   `
 }
