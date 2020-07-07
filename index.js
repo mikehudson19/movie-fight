0f507676dc924da8ebcab49a3f524ba47367487c
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-07 08:32:47 +0200

    Add function to update input field when selection is made and make a new API call

--- index.js
+++ index.js
@@ -31,9 +31,12 @@ const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
 
 
-
 const onInput = async (e) => {
   const movies = await fetchData(e.target.value);
+  if (movies.length == 0) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
 
   // Clear the dropdown each time a search is made
   resultsWrapper.innerHTML = '';
@@ -41,33 +44,35 @@ const onInput = async (e) => {
   // Add the 'is-active' class to display the dropdown menu
   dropdown.classList.add('is-active');
 
-  for (obj of movies) {
+  for (let movie of movies) {
     // Create a dropdown item for each element in the 'movies' array
     const option = document.createElement('a');
     option.classList.add('dropdown-item');
     // Check to see if the image source is valid - set it to an empty string if not
-    const imgSRC = obj.Poster === 'N/A' ? '' : obj.Poster; 
+    const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster; 
     option.innerHTML = `
       <img src="${imgSRC}"/>
-      ${obj.Title} 
+      ${movie.Title} 
     `
+    option.addEventListener('click', () => {
+      input.value = movie.Title;
+      dropdown.classList.remove('is-active');
+      getMoreData(movie);
+    })
     // Append each dropdown item to the UI
    resultsWrapper.appendChild(option);
   }
 
- 
+
 }
 
-input.addEventListener('input', debounce(onInput, 1000))
+input.addEventListener('input', debounce(onInput, 700))
 
 // Function to clear the dropdown when clicked away from
 // document.addEventListener('click', (e) => {
-//   console.log(e.target);
- 
 //   if (e.target.className !== 'dropdown-item') {
 //     resultsWrapper.innerHTML = '';
 //   }
-  
 // })
 
 // Steven's solution to clear the dropdown when clicked away from
@@ -76,3 +81,20 @@ document.addEventListener('click', (e) => {
     dropdown.classList.remove('is-active')
   }
 })
+
+const getMoreData = async (movie) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '94dd967b',
+      i: movie.imdbID
+    }
+  })
+  updateUI(response.data);
+}
+
+function updateUI (movieDetail) {
+  const movieInfo = document.querySelector('.movie-info');
+  movieInfo.innerHTML = `
+
+  `
+}
