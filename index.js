35aff0800d2688a6f77ee71ca7f3c3f6315c1da5
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-08 10:02:45 +0200

    Finish refactor and add comparison functionality.

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ createAutoComplete({
   onOptionSelect(movie) {
     const tutorial = document.querySelector('.tutorial');
     tutorial.classList.add('is-hidden')
-    getMoreData(movie, document.querySelector('#left-summary'));
+    getMoreData(movie, document.querySelector('#left-summary'), 'left');
   },
 })
 
@@ -42,11 +42,14 @@ createAutoComplete({
   onOptionSelect(movie) {
     const tutorial = document.querySelector('.tutorial');
     tutorial.classList.add('is-hidden')
-    getMoreData(movie, document.querySelector('#right-summary'));
+    getMoreData(movie, document.querySelector('#right-summary', 'right'));
   },
 })
 
-const getMoreData = async (movie, summaryElement) => {
+let leftMovie;
+let rightMovie;
+
+const getMoreData = async (movie, summaryElement, side) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '94dd967b',
@@ -54,9 +57,54 @@ const getMoreData = async (movie, summaryElement) => {
     }
   })
   summaryElement.innerHTML = updateUI(response.data);
+
+  if (side === 'left') {
+    leftMovie = response.data;
+  } else {
+    rightMovie = response.data;
+  }
+
+  if (leftMovie && rightMovie) {
+    runComparison();
+  }
+}
+
+const runComparison = () => {
+  const leftSideStats = document.querySelectorAll('#right-summary .notification');
+  const rightSideStats = document.querySelectorAll('#left-summary .notification');
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    const leftSideValue = leftStat.dataset.value;
+    const rightSideValue = rightStat.dataset.value;
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove('is-primary');
+      leftStat.classList.add('is-warning');
+    } else {
+      rightStat.classList.remove('is-primary');
+      rightStat.classList.add('is-warning');
+    }
+  })
 }
 
 function updateUI (movieDetail) {
+  const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+  const metascore = parseInt(movieDetail.Metascore);
+  const imdbRating = parseFloat(movieDetail.imdbRating);
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+  
+  const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+    const value = parseInt(word);
+
+    if (isNaN(value)) {
+      return prev;
+    } else {
+      return prev + value;
+    }
+  }, 0)
+
   return `
   <article class="media">
     <figure class="media-left">
@@ -72,23 +120,23 @@ function updateUI (movieDetail) {
     </div>
   </div>
 </article>
-<article class="notification is-primary">
+<article data-value=${awards} class="notification is-primary">
   <p class="title">${movieDetail.Awards}</p>
   <p class="subtitle">Awards</p>
 </article> 
-<article class="notification is-primary">
+<article data-value=${dollars} class="notification is-primary">
   <p class="title">${movieDetail.BoxOffie}</p>
   <p class="subtitle">Box Office</p>
 </article> 
-<article class="notification is-primary">
+<article data-value=${metascore} class="notification is-primary">
   <p class="title">${movieDetail.Metascore}</p>
   <p class="subtitle">Metascore</p>
 </article> 
-<article class="notification is-primary">
+<article data-value=${imdbRating} class="notification is-primary">
   <p class="title">${movieDetail.imdbRating}</p>
   <p class="subtitle">IMDB Rating</p>
 </article> 
-<article class="notification is-primary">
+<article data-value=${imdbVotes} class="notification is-primary">
   <p class="title">${movieDetail.imdbVotes}</p>
   <p class="subtitle">IMDB Votes</p>
 </article> 
